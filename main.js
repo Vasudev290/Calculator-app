16aa24903a31c48699d96ba818b6dc42afef34c2
vasudev290 <vasukesavulu@gmail.com>
2024-04-01 10:46:59 +0530

    test completed

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ let buttons = document.querySelector(".buttons");
 let btn = document.querySelectorAll("span");
 let value = document.getElementById("value");
 let toggleBtn = document.querySelector(".toggleBtn");
-let body = document.querySelector(".body");
+let body = document.querySelector("body");
 
 for (let i = 0; i < btn.length; i++ ) {
     btn[i].addEventListener("click", function() {
         if(this.innerHTML == "=") {
             value.innerHTML = eval(value.innerHTML);
         }
-
+ 
         else{
-            if (this.innerHTML == "clear") {
+            if (this.innerHTML == "Clear") {
                 value.innerHTML = "";
             }
 
